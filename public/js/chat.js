5e63e5146c37b8c88ea39327089c6626ec1703d1
anamhussain2601 <anamhussain2601@gmail.com>
2020-09-01 00:50:06 +0530

    form and button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,23 @@
 const socket = io()
 
-document.querySelector('#message-form').addEventListener('submit',(e)=>{
+//elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocation = document.querySelector('#send-location')
+
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
+    //disable
     const message = e.target.elements.message.value
-    socket.emit('sendMessage', message, (error)=>{
-        console.log('The message was delivered!',message)
-        if(error) {
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value =''
+        $messageFormInput.focus()
+        if (error) {
             return console.log(error)
         }
         console.log('Message delivered!')
@@ -13,22 +25,25 @@ document.querySelector('#message-form').addEventListener('submit',(e)=>{
 })
 
 
-socket.on('message',(message)=>{
+socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#send-location').addEventListener('click',()=>{
-    if(!navigator.geolocation) {
+$sendLocation.addEventListener('click', () => {
+    if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.')
     }
+
+    $sendLocation.setAttribute('disabled','disabled')
 
     navigator.geolocation.getCurrentPosition((position) => {
         console.log(position)
         socket.emit('sendLocation', {
             latitude: position.coords.longitude,
             longitude: position.coords.longitude
-        },() => {
+        }, () => {
             console.log('Location Shared!')
+            $sendLocation.removeAttribute('disabled')
         })
 
     })
