b4f11d5260fa9422103431c8ebf70c3ba9ec949f
anamhussain2601 <anamhussain2601@gmail.com>
2020-09-04 23:07:51 +0530

    rendering user list

--- src/index.js
+++ src/index.js
@@ -34,7 +34,11 @@ io.on('connection', (socket) => {
         socket.join(user.room)
         socket.emit('message', generateMessage('Admin','Welcome!'))
         socket.broadcast.to(user.room).emit('message', generateMessage('Admin',`${user.username} has joined!`))
-        
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+
+        })
     })
 
     socket.on('sendMessage',(message, callback)=>{
@@ -56,7 +60,11 @@ io.on('connection', (socket) => {
 
         if( user) {
             io.to(user.room).emit('message',generateMessage('Admin',`${user.username} has left`))
+io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
 
+        })
         }
  
     })
